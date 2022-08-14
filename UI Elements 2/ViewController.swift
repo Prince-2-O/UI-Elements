b9eedd76f2f5511cf448447e438100d792ed2da2
Prince-2-O <PrinceTwoPointO@Gmail.com>
2022-08-14 14:01:27 +0530

    Initial Commit

---
+++ UI Elements 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI Elements 2
+//
+//  Created by Prince 2.O on 14/08/22.
+//  Copyright © 2022 Prince 2.O. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
